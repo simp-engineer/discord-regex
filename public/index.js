43048ecc3341430e3c728403c66968fccf1118c7
Leon <147926419+simp-engineer@users.noreply.github.com>
2023-12-25 22:12:39 +0200

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,108 @@
+import { generateLeetspeakRegex } from "../generators.js";
+
+const output = document.getElementById("regex-output")
+const input = document.getElementById("word-input")
+const error_length = document.getElementById("regex-error-toolong")
+const reset_settings = document.getElementById("reset-settings")
+const copy_output = document.getElementById("copy-regex")
+
+const checkbox_num = document.getElementById("filter-num")
+const checkbox_sym = document.getElementById("filter-sym")
+const checkbox_let = document.getElementById("filter-let")
+const checkbox_emo = document.getElementById("filter-emo")
+const checkbox_dub = document.getElementById("double-spm")
+const checkbox_mul = document.getElementById("multi-char")
+const checkbox_vow = document.getElementById("vowel-less")
+const checkbox_whi = document.getElementById("whitespace")
+const checkbox_pmc = document.getElementById("part-match")
+const checkbox_duc = document.getElementById("dedup-char")
+const checkbox_uni = document.getElementById("filter-uni")
+
+function updateRegex() {
+    copy_output.innerText = "Copy"
+    var settings = 0
+    settings += checkbox_num.checked << 0
+    settings += checkbox_sym.checked << 1
+    settings += checkbox_let.checked << 2
+    settings += checkbox_emo.checked << 3
+    settings += checkbox_dub.checked << 4
+    settings += checkbox_mul.checked << 5
+    settings += checkbox_whi.checked << 6
+    settings += checkbox_vow.checked << 7
+    settings += checkbox_pmc.checked << 8
+    settings += checkbox_duc.checked << 9
+    settings += checkbox_uni.checked << 10
+
+    location.hash = btoa(JSON.stringify({
+        input: input.value.toLowerCase(),
+        settings: settings
+    })).replaceAll('+', '-').replaceAll('/', '_').replaceAll('=', '')
+
+    let start_text;
+    if (!input.value == "") {
+        start_text = input.value.toLowerCase();
+    } else {
+        start_text = "hello world";
+    };
+
+    let generated = generateLeetspeakRegex(start_text, settings)
+
+    error_length.hidden = !generated.too_long
+    output.innerText = generated.regex;
+}
+
+reset_settings.onclick = function() {
+    checkbox_num.checked = true;
+    checkbox_sym.checked = true;
+    checkbox_let.checked = true;
+    checkbox_emo.checked = true;
+    checkbox_dub.checked = true;
+    checkbox_mul.checked = false;
+    checkbox_whi.checked = false;
+    checkbox_vow.checked = false;
+    checkbox_pmc.checked = true;
+    checkbox_duc.checked = false;
+    checkbox_uni.checked = false;
+    updateRegex();
+};
+
+copy_output.onclick = function() {
+    navigator.clipboard.writeText(output.innerText);
+    copy_output.innerText = "Copied!";
+    setTimeout(function() {
+        copy_output.innerText = "Copy"
+    }, 5000);
+}
+
+input.oninput = updateRegex
+checkbox_num.onchange = updateRegex
+checkbox_sym.onchange = updateRegex
+checkbox_let.onchange = updateRegex
+checkbox_emo.onchange = updateRegex
+checkbox_dub.onchange = updateRegex
+checkbox_mul.onchange = updateRegex
+checkbox_whi.onchange = updateRegex
+checkbox_vow.onchange = updateRegex
+checkbox_pmc.onchange = updateRegex
+checkbox_duc.onchange = updateRegex
+checkbox_uni.onchange = updateRegex
+
+if (location.hash.length > 1) {
+    const data = JSON.parse(atob(location.hash.replace("#", "").replace('_', '/').replace('-', '+')));
+
+    input.value = data.input;
+
+    checkbox_num.checked = data.settings & 1
+    checkbox_sym.checked = data.settings & 2
+    checkbox_let.checked = data.settings & 4
+    checkbox_emo.checked = data.settings & 8
+    checkbox_dub.checked = data.settings & 16
+    checkbox_mul.checked = data.settings & 32
+    checkbox_whi.checked = data.settings & 64
+    checkbox_vow.checked = data.settings & 128
+    checkbox_pmc.checked = data.settings & 256
+    checkbox_duc.checked = data.settings & 512
+    checkbox_uni.checked = data.settings & 1024
+}
+
+updateRegex()
---
+++ public/regex.js
@@ -0,0 +1,145 @@
+const outputZalgo = document.getElementById("regex-output-zalgo");
+const outputEmojiValue = document.getElementById("emoji-range-value");
+const outputEmoji = document.getElementById("regex-output-emoji");
+const outputLinks = document.getElementById("regex-output-links");
+const outputInvit = document.getElementById("regex-output-invite-links");
+const outputInlin = document.getElementById("regex-output-inline-links");
+const outputHeadnValue = document.getElementById("headn-range-value");
+const outputHeadn = document.getElementById("regex-output-headn");
+
+const copyZalgo = document.getElementById("copy-regex-zalgo")
+const copyEmoji = document.getElementById("copy-regex-emoji")
+const copyLinks = document.getElementById("copy-regex-links")
+const copyInvit = document.getElementById("copy-regex-invite-links")
+const copyInlin = document.getElementById("copy-regex-inline-links")
+const copyHeadn = document.getElementById("copy-regex-headn")
+
+const checkbox_acc = document.getElementById("filter-accents");
+const emoji_slider = document.getElementById("emoji-range");
+const headn_slider = document.getElementById("headn-range");
+const inc_nonclick = document.getElementById("include-nonclickable");
+const inc_3prtyinv = document.getElementById("include-3rdparty");
+
+
+function updateRegexZalgo() {
+    copyZalgo.innerText = "Copy"
+    if (checkbox_acc.checked) {
+        outputZalgo.innerText = "\\p{M}{1,}";
+    } else {
+        outputZalgo.innerText = "\\p{M}{3,}";
+    };
+};
+
+copyZalgo.onclick = function() {
+    navigator.clipboard.writeText(outputZalgo.innerText);
+    copyZalgo.innerText = "Copied!";
+    setTimeout(function() {
+        copyZalgo.innerText = "Copy"
+    }, 5000);
+}
+
+checkbox_acc.onchange = updateRegexZalgo;
+updateRegexZalgo();
+
+function updateRegexEmoji() {
+    copyEmoji.innerText = "Copy"
+    outputEmojiValue.innerText = emoji_slider.value
+    if (emoji_slider.value == 0) {
+        outputEmoji.innerText = `<a?:[a-z_0-9]+:[0-9]+>|\\p{Extended_Pictographic}|\\p{Emoji}`
+    } else {
+        outputEmoji.innerText = `((?s)(<a?:[a-z_0-9]+:[0-9]+>|\\p{Extended_Pictographic}|\\p{Emoji}).*){${+emoji_slider.value + 1},}`
+    }
+};
+
+copyEmoji.onclick = function() {
+    navigator.clipboard.writeText(outputEmoji.innerText);
+    copyEmoji.innerText = "Copied!";
+    setTimeout(function() {
+        copyEmoji.innerText = "Copy"
+    }, 5000);
+}
+
+emoji_slider.onchange = updateRegexEmoji
+emoji_slider.oninput = updateRegexEmoji
+updateRegexEmoji()
+
+function updateRegexLinks() {
+    copyLinks.innerText = "Copy"
+    if (inc_nonclick.checked) {
+        outputLinks.innerText = "(?:https?://)?[a-z0-9_\\-\\.]*[a-z0-9_\\-]+\\.[a-z]{2,}";
+    } else {
+        outputLinks.innerText = "(?:https?://)[a-z0-9_\\-\\.]*[a-z0-9_\\-]+\\.[a-z]{2,}";
+    };
+};
+
+copyLinks.onclick = function() {
+    navigator.clipboard.writeText(outputLinks.innerText);
+    copyLinks.innerText = "Copied!";
+    setTimeout(function() {
+        copyLinks.innerText = "Copy"
+    }, 5000);
+}
+
+inc_nonclick.onchange = updateRegexLinks;
+updateRegexLinks();
+
+function updateRegexInlin() {
+    copyInlin.innerText = "Copy"
+    outputInlin.innerText = "\\[.*\\]\\(<?(?:https?://)?[a-z0-9_\\-\\.]*[a-z0-9_\\-]+\\.[a-z]{2,}.*>?\\)";
+};
+
+copyInlin.onclick = function() {
+    navigator.clipboard.writeText(outputInlin.innerText);
+    copyInlin.innerText = "Copied!";
+    setTimeout(function() {
+        copyInlin.innerText = "Copy"
+    }, 5000);
+}
+
+updateRegexInlin();
+
+function updateRegexInvit() {
+    copyInvit.innerText = "Copy"
+    if (inc_3prtyinv.checked) {
+        outputInvit.innerText = "(?:https?://)?(?:www.|ptb.|canary.)?(?:discord(?:app)?\.(?:(?:com|gg)/(?:invite|servers)/[a-z0-9-_]+)|discord\.gg/[a-z0-9-_]+)|(?:https?://)?(?:www\.)?(?:dsc\.gg|invite\.gg+|discord\.link|(?:discord\.(gg|io|me|li|id))|disboard\.org)/[a-z0-9-_/]+"
+    } else {
+        outputInvit.innerText = "(?:https?://)?(?:www.|ptb.|canary.)?(?:discord(?:app)?\\.(?:(?:com|gg)/(?:invite|servers)/[a-z0-9-_]+)|discord\\.gg/[a-z0-9-_]+)";
+    }
+};
+
+copyInvit.onclick = function() {
+    navigator.clipboard.writeText(outputInvit.innerText);
+    copyInvit.innerText = "Copied!";
+    setTimeout(function() {
+        copyInvit.innerText = "Copy"
+    }, 5000);
+}
+
+updateRegexInvit();
+inc_3prtyinv.onchange = updateRegexInvit
+
+function updateRegexHeadn() {
+    copyHeadn.innerText = "Copy";
+    if (headn_slider.value == 4) {
+        outputHeadnValue.innerText = "-";
+    } else {
+        outputHeadnValue.innerText = headn_slider.value;
+    };
+    if (headn_slider.value == 2) {
+        outputHeadn.innerText = `^#\\s.*$`;
+    } else {
+        outputHeadn.innerText = `^#{1,${headn_slider.value - 1}}\\s.*$`;
+    }
+};
+
+copyHeadn.onclick = function() {
+    navigator.clipboard.writeText(outputHeadn.innerText);
+    copyHeadn.innerText = "Copied!";
+    setTimeout(function() {
+        copyHeadn.innerText = "Copy"
+    }, 5000);
+};
+
+headn_slider.onchange = updateRegexHeadn;
+headn_slider.oninput = updateRegexHeadn;
+updateRegexHeadn();
